6b0ca554e0d91034f27899053049cc8acf76fd67
libjared <libjared@users.noreply.github.com>
2022-04-18 14:50:27 -0500

    Attempt build without helpers

--- flake.nix
+++ flake.nix
@@ -7,11 +7,11 @@
     pkgs = import nixpkgs {
       inherit system;
     };
-    baseName = "monogamedesktopgl";
+    name = "monogamedesktopgl";
 
     nixlessSource = pkgs.lib.sources.cleanSourceWith {
+      inherit name;
       src = self;
-      name = baseName;
       filter = name: type: let baseFileName = baseNameOf (toString name); in ! (
         pkgs.lib.hasSuffix ".nix" baseFileName ||
         baseFileName == "flake.lock"
@@ -19,15 +19,36 @@
     };
 
     monogamedesktopgl = pkgs.stdenvNoCC.mkDerivation {
-      inherit baseName;
+      inherit name;
       src = nixlessSource;
       version = "3.8.0.1642";
+      nativeBuildInputs = [
+        pkgs.dotnet-sdk
+      ];
+      projectFile = "MonoGame.Framework.DesktopGL.csproj";
+
+      # nugetDeps = linkFarmFromDrvs "${name}-nuget-deps" (import ./nuget-deps.nix {
+      #   fetchNuGet = { name, version, sha256 }: (let
+      #     lowerName = lib.toLower name;
+      #     in (fetchurl {
+      #       name = "${lowerName}.${version}.nupkg";
+      #       url = "https://www.nuget.org/api/v2/package/${name}/${version}";
+      #       inherit sha256;
+      #     })
+      #   );
+      # });
+
+      configurePhase = ''
+        cd "MonoGame.Framework"
+        #dotnet restore --source "$nugetDeps" "$projectFile"
+        dotnet restore "$projectFile"
+      '';
 
       buildPhase = ''
-        dotnet pack
+        cd "MonoGame.Framework"
+        dotnet pack --no-restore "$projectFile"
       '';
     };
-
   in {
     packages.x86_64-linux = {
       inherit monogamedesktopgl;
---
+++ nuget-deps.nix
@@ -0,0 +1,9 @@
+{ fetchNuGet }: [
+  (fetchNuGet { name = "NETStandard.Library"; version = "3.1.10"; sha256 = "sha256-51D1XkqFMPHJzOmt1HQ0Bf1n9K0auwEyxTJuqA/8xHY="; })
+  (fetchNuGet { name = "Microsoft.AspNetCore.App.Runtime.linux-x64"; version = "3.1.23"; sha256 = "sha256-7FMdfLiZJTxno/62uriFf7sgRVIt31v1zKy086C1pRA="; })
+  (fetchNuGet { name = "Microsoft.NETCore.App.Host.linux-x64"; version = "3.1.23"; sha256 = "sha256-05vBtHish3yYihl+LwRtV7ZjI8+9cpH2Zy5XzXU6niU="; })
+  (fetchNuGet { name = "Microsoft.NETCore.App.Ref"; version = "3.1.0"; sha256 = "sha256-nuAvHwmJ2s3Ob1qNDH1+uV3awOZaWlaV3FenTmPUWyM="; })
+  (fetchNuGet { name = "Microsoft.NETCore.App.Runtime.linux-x64"; version = "3.1.23"; sha256 = "sha256-7++UmH0P9df8TU+a1JC783l9rehp2rkciLMSOdUHwl0="; })
+  (fetchNuGet { name = "MonoGame.Content.Builder.Task"; version = "3.8.0.1641"; sha256 = "sha256-2HK5xDZKIpOp15A1VyCIl+pwXOOxqD9ETiQqpCJtgqc="; })
+  (fetchNuGet { name = "MonoGame.Framework.DesktopGL"; version = "3.8.0.1641"; sha256 = "sha256-XX8veHU/7f+nbmHexctW3dt0rFGeAnciX354+x29MlY="; })
+]
