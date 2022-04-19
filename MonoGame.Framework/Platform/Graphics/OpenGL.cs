65b66f0b1af3b296d0def1fdacb84d491d481d27
libjared <libjared@users.noreply.github.com>
2022-04-19 17:40:22 -0500

    Revert "Add debug tracing 3"
    This reverts commit ce06ce5a40632474efe752c4a0f7ddfb69443742.

--- MonoGame.Framework/Platform/Graphics/OpenGL.cs
+++ MonoGame.Framework/Platform/Graphics/OpenGL.cs
@@ -1277,7 +1277,7 @@ namespace MonoGame.OpenGL
             EnableVertexAttribArray = LoadFunction<EnableVertexAttribArrayDelegate> ("glEnableVertexAttribArray");
             DisableVertexAttribArray = LoadFunction<DisableVertexAttribArrayDelegate> ("glDisableVertexAttribArray");
             GetIntegerv = LoadFunction<GetIntegerDelegate> ("glGetIntegerv");
-            GetStringInternal = LoadFunction<GetStringDelegate> ("glGetString", true);
+            GetStringInternal = LoadFunction<GetStringDelegate> ("glGetString");
             ClearDepth = LoadFunction<ClearDepthDelegate> ("glClearDepth");
             if (ClearDepth == null)
                 ClearDepth = LoadFunction<ClearDepthDelegate> ("glClearDepthf");
@@ -1530,8 +1530,7 @@ namespace MonoGame.OpenGL
 
         internal unsafe static string GetString (StringName name)
         {
-            Console.WriteLine("GetString called with: " + name + "!");
-            Console.WriteLine("GetStringInternal is: " + GetStringInternal + "!");
+            Console.WriteLine("GetString called with " + name + "!");
             IntPtr ptrResult = GetStringInternal (name);
             Console.WriteLine("Got a pointer: " + ptrResult + "!");
             string result = Marshal.PtrToStringAnsi (ptrResult);
